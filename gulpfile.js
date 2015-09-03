738d0969e84b520cac0a98a93381082fdc9b2545
Diego Plascencia <diego-d@outlook.com>
2015-09-03 14:11:23 -0500

    gitignore and gulpfile to deploy (jenkyns-git)

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')(); // all plugins stored in '$' var
+var builtdir = process.env.RICHIT_WEB_DIR || 'dest';
+
+gulp.task('clean', function(){
+    return gulp.src(builtdir, {read: false})
+        .pipe($.clean());
+});
+
+gulp.task('upload', function(){
+    return gulp.src('3.9.0/**/*')
+        .pipe(gulp.dest(builtdir));
+});
+
+gulp.task('deploy', ['upload']);
