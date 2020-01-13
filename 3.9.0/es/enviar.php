5177f5129157f5a0926beafe6bb4eda77d777464
ursula9 <urspala@gmail.com>
2020-01-13 17:42:29 -0600

    Prueba de formulario nuevo
    El antiguo formulario que se tenía no enviaba.. se quedaba ahí cargando

---
+++ 3.9.0/es/enviar.php
@@ -0,0 +1,9 @@
+<?php
+  $destino = "upalacios@richit.ai";
+  $nombre = $_Post["nombre"];
+  $correo = $_Post["correo"];
+  $telefono = $_Post["telefono"];
+  $contenido = "Nombre: " . $nombre ."\nCorreo: " .$correo . "\nTeléfono: " .$telefono;
+  mail($destino,"Descarga de brochure", $contenido);
+  header("index.html")
+ ?>
