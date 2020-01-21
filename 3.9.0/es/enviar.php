edc670a01c6a1ea7e8190dbe552e92767896cd0d
ursula9 <urspala@gmail.com>
2020-01-21 17:24:27 -0600

    cambio de correo

--- 3.9.0/es/enviar.php
+++ 3.9.0/es/enviar.php
@@ -1,11 +1,9 @@
 <?php
-  $destino = "urspala@gmail.com";
+  $destino = "upalacios@richit.ai";
   $nombre = $_POST["nombre"];
   $correo = $_POST["correo"];
   $telefono = $_POST["telefono"];
   $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTeléfono: " . $telefono;
   mail($destino,"Descarga de brochure", $contenido);
   header("Location: http://richit.ai/es/");
-  echo"<script type=\"text/javascript\">alert('Su mensaje ha sido enviado con exito, en la brevedad posible nos comunicaremos con usted'); window.location='index.html';</script>";
- 
  ?>
