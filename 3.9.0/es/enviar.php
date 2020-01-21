7d1d65ee85cff09fb4e27d5c17cd112fa2bb60c0
ursula9 <urspala@gmail.com>
2020-01-21 16:55:52 -0600

    Update enviar.php

--- 3.9.0/es/enviar.php
+++ 3.9.0/es/enviar.php
@@ -6,4 +6,6 @@
   $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTeléfono: " . $telefono;
   mail($destino,"Descarga de brochure", $contenido);
   header("Location: http://richit.ai/es/");
+  echo"<script type=\"text/javascript\">alert('Su mensaje ha sido enviado con exito, en la brevedad posible nos comunicaremos con usted'); window.location='index.html';</script>";
+ 
  ?>
