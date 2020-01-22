f24b269c93685a5cda793dda75cd1eb33f222702
ursula9 <urspala@gmail.com>
2020-01-22 10:44:42 -0600

    Update enviar-prueba.php

--- 3.9.0/php/enviar-prueba.php
+++ 3.9.0/php/enviar-prueba.php
@@ -11,8 +11,7 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    //  $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->SMTPDebug = 0;    
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output 
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp1.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
