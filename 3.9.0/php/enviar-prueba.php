9870d3d75f6f6977d67d119554546445a0d87efe
ursula9 <urspala@gmail.com>
2020-01-22 11:21:33 -0600

    Update enviar-prueba.php

--- 3.9.0/php/enviar-prueba.php
+++ 3.9.0/php/enviar-prueba.php
@@ -13,7 +13,7 @@ try {
     //Server settings
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->SMTPDebug = 2; 
-    $mail->isSMTP();                                            // Send using SMTP
+    //$mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp1.gmail.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->Username   = 'urspala@gmail.com';                     // SMTP username
