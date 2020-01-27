39acd6a4201c30e75f148295edcd87f5082179fc
ursula9 <urspala@gmail.com>
2020-01-27 12:30:27 -0600

    Update validarFormulario.js

--- 3.9.0/js/validarFormulario.js
+++ 3.9.0/js/validarFormulario.js
@@ -2,8 +2,29 @@ function validar(){
   var nombre, correo;
   nombre = document.getElementById("nombre").value;
   correo = document.getElementById("correo").value;
+
+  expresion = /\w+@\w+\.+[a-z]/;
+
   if(nombre === "" ||  correo === ""){
     alert("El campo Nombre y Correo son obligatorios");
     return false;
   }
+  else if(nombre.length>120){
+    alert("El nombre es muy largo");
+    return false;
+  }
+  else if(correo.length>100){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(correo.length>100){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(!expresion.test(correo)){
+    alert("El correo no es valido");
+    return false;
+  }
+
+
 }
