c6987a45629b6e2cba583e3b07d3db41faf17e8b
ursula9 <urspala@gmail.com>
2020-01-28 14:54:54 -0600

    jquery boton

---
+++ 3.9.0/js/formularioDownload.js
@@ -0,0 +1,7 @@
+$( "#form0" ).submit(function( event ) {
+  alert( " form0 Handler for .submit() called." );
+});
+
+$( "#form1" ).submit(function( event ) {
+  alert( " form1 Handler for .submit() called." );
+});
