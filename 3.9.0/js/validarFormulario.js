5a933eafbd1047527f95a8f8b7f482e7f3a059d8
ursula9 <urspala@gmail.com>
2020-01-28 10:54:26 -0600

    js descargable

--- 3.9.0/js/validarFormulario.js
+++ 3.9.0/js/validarFormulario.js
@@ -25,5 +25,18 @@ function validar(){
     alert("El correo no es valido");
     return false;
   }
-  alert('clicked urs');
+
+  document.getElementById('form0').addEventListener('submit',function(){
+    console.log('form 0');
+  });
+
+  document.getElementById('form1').addEventListener('submit',function(){
+    console.log('form 1');
+  });
+
+  document.getElementById('form2').addEventListener('submit',function(){
+    console.log('form 2');
+  });
+
+
 }
