7df0132ec85c02cc4d4a53c212673ee1ccbaeddd
ursula9 <urspala@gmail.com>
2020-01-28 10:36:37 -0600

    php jscript formulario

--- 3.9.0/js/formularioDownload.js
+++ 3.9.0/js/formularioDownload.js
@@ -1,3 +1,11 @@
 document.getElementById('form0').addEventListener('submit',function(){
-  alert('clicked urs');
+  console.log('form 0')
+});
+
+document.getElementById('form1').addEventListener('submit',function(){
+  console.log('form 1')
+});
+
+document.getElementById('form2').addEventListener('submit',function(){
+  console.log('form 2')
 });
