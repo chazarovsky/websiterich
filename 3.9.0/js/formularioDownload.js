b5f5115249a00163b26235d64f1ae07fa92d56b8
ursula9 <urspala@gmail.com>
2020-01-28 17:44:10 -0600

    id del boton

--- 3.9.0/js/formularioDownload.js
+++ 3.9.0/js/formularioDownload.js
@@ -1,1 +1,26 @@
 
+$(document).ready(function(){
+  $("#form0").submit(function() {
+    alert( " form0 Handler for .submit() called." );
+  });
+
+  $("#boton1").submit(function() {
+    $.ajax({
+        url: '../images/slider.jpg',
+        method: 'GET',
+        xhrFields: {
+            responseType: 'blob'
+        },
+        success: function (data) {
+            var a = document.createElement('a');
+            var url = window.URL.createObjectURL(data);
+            a.href = url;
+            a.download = 'slider.jpg';
+            document.body.append(a);
+            a.click();
+            a.remove();
+            window.URL.revokeObjectURL(url);
+        }
+    });
+  });
+});
