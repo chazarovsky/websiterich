aa3edbf8586088707c66333ad698d38e2b801a21
ursula9 <urspala@gmail.com>
2020-01-28 15:21:06 -0600

    Update formularioDownload.js

--- 3.9.0/js/formularioDownload.js
+++ 3.9.0/js/formularioDownload.js
@@ -1,7 +1,12 @@
-$("#form0").submit(function() {
-  alert( " form0 Handler for .submit() called." );
-});
 
-$("#form1").submit(function() {
-  alert( " form1 Handler for .submit() called." );
+
+$(document).ready(function(){
+  $("#form0").submit(function() {
+    alert( " form0 Handler for .submit() called." );
+  });
+
+  $("#form1").submit(function() {
+    alert( " form1 Handler for .submit() called." );
+  });
+  
 });
