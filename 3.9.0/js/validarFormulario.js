bb8ae0458fdfacbc13dc35d8fc19ed70fb770a68
ursula9 <urspala@gmail.com>
2020-01-28 12:08:53 -0600

    Update validarFormulario.js

--- 3.9.0/js/validarFormulario.js
+++ 3.9.0/js/validarFormulario.js
@@ -26,9 +26,11 @@ function validar(){
     return false;
   }
 
-  const form0 = document.getElementById("form0");
-  form0.addEventListener("submit",(e) => {
-    e.preventDefault();
-    console.log("form0 is submited");
-  });
+
 }
+
+const form0 = document.getElementById("form0");
+form0.addEventListener("submit",(e) => {
+  e.preventDefault();
+  console.log("form0 is submited");
+});
