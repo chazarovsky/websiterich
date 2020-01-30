726f410594711e19e689b0cd709844c00c155e73
ursula9 <urspala@gmail.com>
2020-01-30 16:19:19 -0600

    Update formularioDownload.js

--- 3.9.0/js/formularioDownload.js
+++ 3.9.0/js/formularioDownload.js
@@ -31,23 +31,7 @@ $(document).ready(function(){
 $(document).ready(function(){
 
   $("#form0").submit(function() {
-    $.ajax({
-        url: '../images/slider.jpg',
-        method: 'GET',
-        xhrFields: {
-            responseType: 'blob'
-        },
-        success: function (data) {
-            var a = document.createElement('a');
-            var url = window.URL.createObjectURL(data);
-            a.href = url;
-            a.download = 'slider.jpg';
-            document.body.append(a);
-            a.click();
-            a.remove();
-            window.URL.revokeObjectURL(url);
-        }
-    });
+     window.location = '../images/slider.jpg';
   });
   $("#form1").submit(function() {
     $.ajax({
