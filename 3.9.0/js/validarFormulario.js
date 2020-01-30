e4f24ac199ed78cc5cfd2150a4eec0a14e0d87a9
ursula9 <urspala@gmail.com>
2020-01-30 12:15:39 -0600

    validaciones 0 y 1

--- 3.9.0/js/validarFormulario.js
+++ 3.9.0/js/validarFormulario.js
@@ -1,4 +1,4 @@
-function validar(){
+function validar0(){
   var nombre, correo;
   nombre = document.getElementById("nombre").value;
   correo = document.getElementById("correo").value;
@@ -25,5 +25,33 @@ function validar(){
     alert("El correo no es valido");
     return false;
   }
+}
 
+function validar1(){
+  var nombre, correo;
+  nombre = document.getElementById("nombre").value;
+  correo = document.getElementById("correo").value;
+
+  expresion = /\w+@\w+\.+[a-z]/;
+
+  if(nombre === "" ||  correo === ""){
+    alert("El campo Nombre y Correo son obligatorios");
+    return false;
+  }
+  else if(nombre.length>120){
+    alert("El nombre es muy largo");
+    return false;
+  }
+  else if(correo.length>100){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(correo.length>100){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(!expresion.test(correo)){
+    alert("El correo no es valido");
+    return false;
+  }
 }
