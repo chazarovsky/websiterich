c8311e99a10e15a570ebf17ed5e7ff6e3fe07fa8
ursula9 <urspala@gmail.com>
2020-02-04 11:35:20 -0600

    Update voice.js

--- 3.9.0/Retail/js/voice.js
+++ 3.9.0/Retail/js/voice.js
@@ -406,7 +406,7 @@ var args = {
 	arrowRight : '.arrow-right', //A jQuery reference to the right arrow
 	arrowLeft : '.arrow-left', //A jQuery reference to the left arrow
 	speed : 1000, //The speed of the animation (milliseconds)
-	slideDuration : 4000 //The amount of time between animations (milliseconds)
+	slideDuration : 10000 //The amount of time between animations (milliseconds)
 };
 
 $('.carousel').Zippy(args);
