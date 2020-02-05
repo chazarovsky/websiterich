54a8c455b43485d10ce3d74bcf179627e3495c81
ursula9 <urspala@gmail.com>
2020-02-05 09:59:25 -0600

    RSP Conversion tracking prueba 1

--- 3.9.0/js/validarFormulario.js
+++ 3.9.0/js/validarFormulario.js
@@ -1,3 +1,32 @@
+function validarRSP(){
+  var nombre, correo;
+  nombre = document.getElementById("nombreRSP").value;
+  correo = document.getElementById("correoRSP").value;
+
+  expresion = /\w+@\w+\.+[a-z]/;
+
+  if(nombre === "" ||  correo === ""){
+    alert("El campo Nombre y Correo son obligatorios");
+    return false;
+  }
+  else if(nombre.length>120){
+    alert("El nombre es muy largo");
+    return false;
+  }
+  else if(correo.length>100){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(correo.length>100){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(!expresion.test(correo)){
+    alert("El correo no es valido");
+    return false;
+  }
+}
+
 function validar0(){
   var nombre, correo;
   nombre = document.getElementById("nombre0").value;
