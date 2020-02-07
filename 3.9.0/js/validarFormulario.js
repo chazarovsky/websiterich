875374ba2c74af5a1cfee430e9f62a90c708ecb0
ursula9 <urspala@gmail.com>
2020-02-07 17:13:18 -0600

    formulario Elastic

--- 3.9.0/js/validarFormulario.js
+++ 3.9.0/js/validarFormulario.js
@@ -85,6 +85,35 @@ function validarDremio(){
   }
 }
 
+function validarElastic(){
+  var nombre, correo;
+  nombre = document.getElementById("nombreElastic").value;
+  correo = document.getElementById("correoElastic").value;
+
+  expresion = /\w+@\w+\.+[a-z]/;
+
+  if(nombre === "" ||  correo === ""){
+    alert("El campo Nombre y Correo son obligatorios");
+    return false;
+  }
+  else if(nombre.length>120){
+    alert("El nombre es muy largo");
+    return false;
+  }
+  else if(correo.length>100){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(correo.length>100){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(!expresion.test(correo)){
+    alert("El correo no es valido");
+    return false;
+  }
+}
+
 function validar0(){
   var nombre, correo;
   nombre = document.getElementById("nombre0").value;
