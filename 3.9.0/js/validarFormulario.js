99f1579176f10c945f2434e6e0d4ff7da7a6f66a
ursula9 <urspala@gmail.com>
2020-02-07 17:21:32 -0600

    Formulario Kafka

--- 3.9.0/js/validarFormulario.js
+++ 3.9.0/js/validarFormulario.js
@@ -114,6 +114,35 @@ function validarElastic(){
   }
 }
 
+function validarKafka(){
+  var nombre, correo;
+  nombre = document.getElementById("nombreKafka").value;
+  correo = document.getElementById("correoKafka").value;
+
+  expresion = /\w+@\w+\.+[a-z]/;
+
+  if(nombre === "" ||  correo === ""){
+    alert("El campo Nombre y Correo son obligatorios");
+    return false;
+  }
+  else if(nombre.length>120){
+    alert("El nombre es muy largo");
+    return false;
+  }
+  else if(correo.length>100){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(correo.length>100){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(!expresion.test(correo)){
+    alert("El correo no es valido");
+    return false;
+  }
+}
+
 function validar0(){
   var nombre, correo;
   nombre = document.getElementById("nombre0").value;
