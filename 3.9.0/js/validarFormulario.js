0bc3b1bef25f609af86979b0908038134b7445c3
ursula9 <urspala@gmail.com>
2020-02-07 15:21:34 -0600

    Formulario Cloudera

--- 3.9.0/js/validarFormulario.js
+++ 3.9.0/js/validarFormulario.js
@@ -27,6 +27,35 @@ function validarRSP(){
   }
 }
 
+function validarAnalytics(){
+  var nombre, correo;
+  nombre = document.getElementById("nombreAnalytics").value;
+  correo = document.getElementById("correoAnalytics").value;
+
+  expresion = /\w+@\w+\.+[a-z]/;
+
+  if(nombre === "" ||  correo === ""){
+    alert("El campo Nombre y Correo son obligatorios");
+    return false;
+  }
+  else if(nombre.length>120){
+    alert("El nombre es muy largo");
+    return false;
+  }
+  else if(correo.length>100){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(correo.length>100){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(!expresion.test(correo)){
+    alert("El correo no es valido");
+    return false;
+  }
+}
+
 function validar0(){
   var nombre, correo;
   nombre = document.getElementById("nombre0").value;
---
+++ 3.9.0/php/enviar-cloudera.php
@@ -0,0 +1,57 @@
+<?php
+
+$nombre = $_POST["nombreCloudera"];
+$correo = $_POST["correoCloudera"];
+$telefono = $_POST["telefono"];
+
+$contenido = "Nombre: " . $nombre . "<br>Correo: " . $correo . "<br>Teléfono: " . $telefono ;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
+
+$mail = new PHPMailer(true);
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'upalacios@richit.ai';                 // SMTP username
+    $mail->Password = '9juankis9';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('upalacios@richit.ai', 'Formulario proveniente de Campaña de Google Cloudera');
+    $mail->addAddress('echazaro@richit.ai');     // Add a recipient
+    $mail->addAddress('mdeloscobos@richit.ai');               // Name is optional
+    $mail->addAddress('eperezyera@richit.ai');
+    $mail->addAddress('upalacios@richit.ai');
+    //$mail->addAddress('ellen@example.com');               // Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('../images/slider.jpg', 'new.jpg');    // Optional name
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Formulario Web RICHIT';
+    $mail->Body    = $contenido; //'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    //echo 'El mensaje se envió correctamente';
+    header("Location: http://richit.ai/es/cloudera_mexico.html");
+} catch (Exception $e) {
+    echo 'hubo un error al enviarse';
+    echo 'Mailer mando Error: ' . $mail->ErrorInfo;
+}
+
+?>
