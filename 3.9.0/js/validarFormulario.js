318260cdd76ba994d06daff3d2c1847363fdbe97
ursula9 <urspala@gmail.com>
2020-06-26 20:10:16 -0500

    contacto-formulario php en español

--- 3.9.0/js/validarFormulario.js
+++ 3.9.0/js/validarFormulario.js
@@ -1,3 +1,32 @@
+function validarContacto(){
+  var nombre, correo;
+  nombre = document.getElementById("nombreContacto").value;
+  correo = document.getElementById("correoContacto").value;
+
+  expresion = /\w+@\w+\.+[a-z]/;
+
+  if(nombre === "" ||  correo === ""){
+    alert("El campo Nombre y Correo son obligatorios");
+    return false;
+  }
+  else if(nombre.length>60){
+    alert("El nombre es muy largo");
+    return false;
+  }
+  else if(correo.length>50){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(correo.length>50){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(!expresion.test(correo)){
+    alert("El correo no es valido");
+    return false;
+  }
+}
+
 function validarRSP(){
   var nombre, correo;
   nombre = document.getElementById("nombreRSP").value;
