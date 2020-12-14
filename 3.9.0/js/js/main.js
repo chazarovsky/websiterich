cf78b7c878b335efa79333e0cdab1aa3a852591a
Urs <urspala@gmail.com>
2020-12-13 20:51:10 -0600

    index
    se borran html de español y se agregan nuevas carpetas

---
+++ 3.9.0/js/js/main.js
@@ -0,0 +1,53 @@
+/*===== MENU SHOW Y HIDDEN =====*/
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
+
+// SHOW
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
+
+// HIDDEN
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+/*===== MOUSEMOVE HOME IMG =====*/
+document.addEventListener('mousemove', move);
+function move(e){
+    this.querySelectorAll('.move').forEach(layer =>{
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX*speed)/120
+        const y = (window.innerHeight - e.pageY*speed)/120
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
+
+document.addEventListener('mousemove', move_0);
+function move_0(e){
+    this.querySelectorAll('.move_0').forEach(layer =>{
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX*speed)/260
+        const y = (window.innerHeight - e.pageY*speed)/260
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
+
+/*===== GSAP ANIMATION =====*/
+// NAV
+
+gsap.from('.nav__logo, .nav__toggle', {opacity: 0, duration: 0.5, delay:0.5, y: 5})
+gsap.from('.nav__item', {opacity: 0, duration: 1, delay: 0.5, y: 30, stagger: 0.2,})
+
+// HOME
+/*
+gsap.from('.home__title', {opacity: 0, duration: 1, delay:1.6, y: 30})
+gsap.from('.home__description', {opacity: 0, duration: 1, delay:1.8, y: 30})
+gsap.from('.home__button', {opacity: 0, duration: 1, delay:2.1, y: 30})
+gsap.from('.home__img', {opacity: 0, duration: 1, delay:1.3, y: 30})
+*/
