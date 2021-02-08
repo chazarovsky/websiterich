752345136ffa1a7e69d9557aacf91cbfd39449d8
ursula9 <>
2021-02-07 22:55:53 -0600

    formulario richit rsp

--- 3.9.0/js/validarFormulario.js
+++ 3.9.0/js/validarFormulario.js
@@ -27,6 +27,35 @@ function validarContacto(){
   }
 }
 
+function validarContactoRSP(){
+  var nombre, correo;
+  nombre = document.getElementById("nombreContacto-RSP").value;
+  correo = document.getElementById("correoContacto-RSP").value;
+
+  expresion = /\w+@\w+\.+[a-z]/;
+
+  if(nombre === "" ||  correo === ""){
+    alert("El campo Nombre y Correo son obligatorios");
+    return false;
+  }
+  else if(nombre.length>60){
+    alert("El nombre es muy largo");
+    return false;
+  }
+  else if(correo.length>50){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(correo.length>50){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(!expresion.test(correo)){
+    alert("El correo no es valido");
+    return false;
+  }
+}
+
 function validarRSP(){
   var nombre, correo;
   nombre = document.getElementById("nombreRSP").value;
