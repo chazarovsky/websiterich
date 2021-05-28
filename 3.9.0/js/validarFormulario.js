b2500af39a067924901133454d8ec7bc411bb45e
ursula9 <urspala@gmail.com>
2021-05-27 21:51:44 -0500

    video isai

--- 3.9.0/js/validarFormulario.js
+++ 3.9.0/js/validarFormulario.js
@@ -868,3 +868,33 @@ function validar14(){
     return false;
   }
 }
+
+
+function validar15(){
+  var nombre, correo;
+  nombre = document.getElementById("nombre15").value;
+  correo = document.getElementById("correo15").value;
+
+  expresion = /\w+@\w+\.+[a-z]/;
+
+  if(nombre === "" ||  correo === ""){
+    alert("El campo Nombre y Correo son obligatorios");
+    return false;
+  }
+  else if(nombre.length>60){
+    alert("El nombre es muy largo");
+    return false;
+  }
+  else if(correo.length>50){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(correo.length>60){
+    alert("El correo es muy largo");
+    return false;
+  }
+  else if(!expresion.test(correo)){
+    alert("El correo no es valido");
+    return false;
+  }
+}
